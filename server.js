9e61b351ce1939390fce5cf5321017273ac21ce5
Alex <alex.curci@gmail.com>
2017-01-09 13:16:43 -0500

    Update server.js
    Updated quotation marks on console.log line 13.

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.static('public'));
 //});
 
 var server = app.listen(8081, function() {
-  console.log('Node app running on port 8081.');
+  console.log("Node app running on port 8081.");
 
 });
 
